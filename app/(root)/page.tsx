f49625603680d6bd357b4d8b40038bc18356895b
Gerson Rocha <gersonrocha9@gmail.com>
2023-07-14 14:08:30 -0300

    feat: create modal component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,22 @@
+'use client'
+
 import { UserButton } from '@clerk/nextjs'
+
+import { Modal } from '@/components/ui/modal'
 
 export default function SetupPage() {
   return (
     <div className="p-4">
       <UserButton afterSignOutUrl="/" />
+
+      <Modal
+        title="Modal"
+        description="This is a modal"
+        isOpen
+        onClose={() => {}}
+      >
+        Modal content
+      </Modal>
     </div>
   )
 }
